264dd3d3cff26f696bca159a83545f41eab490b5
Purabsh93 <purab.shah@airpay.co.in>
2019-07-10 18:52:16 +0530

    Updates

---
+++ precache-manifest.9d54c1ce131f162f309fbc0353a42933.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650b9e067c8eca1574972ba2f3a3cf12",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b94d0c2d3f629c0284a9",
+    "url": "/myapp/static/css/2.b8c95347.chunk.css"
+  },
+  {
+    "revision": "20a8759294f60bd3e8b8",
+    "url": "/myapp/static/css/main.612d4738.chunk.css"
+  },
+  {
+    "revision": "b94d0c2d3f629c0284a9",
+    "url": "/myapp/static/js/2.8c3972f8.chunk.js"
+  },
+  {
+    "revision": "20a8759294f60bd3e8b8",
+    "url": "/myapp/static/js/main.e75dfe95.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/myapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/myapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/myapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/myapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/myapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
